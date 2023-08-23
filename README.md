fe505d1f1dd4cfac02b3569fc01c8ea7327b52bf
Dave Harding <daveharding@outlook.com>
2023-08-23 15:49:20 -0700

    Added bottom tab navigation and icons (#2)

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # RentIt
+
+
+Dependencies
+
+npx expo install @react-navigation/bottom-tabs 
+npx expo install @react-navigation/native
+
+Extentions
+
+Prettier
+Live Share
---
+++ screens/filter.js
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function Filter() {
+    return (
+        <SafeAreaView>
+            <Text> Filter </Text>
+        </SafeAreaView>
+    );
+}
